2df1d6f9f076479c7b79bdd4acc25b9cd18f7068
Guilherme Onizio <yguih011@gmail.com>
2025-01-29 16:12:29 -0300

    created entity Cliente and repository

---
+++ src/main/java/com/guilhermeonizio/backend_pizzaria/BackendPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeonizio.backend_pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeonizio/backend_pizzaria/BackendPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeonizio.backend_pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
